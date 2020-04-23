faaef8e1ad45acb3146268834731fba595963051
wizi <rene.witzelsperger@karriere.at>
2020-04-23 10:36:56 +0200

    Init project

---
+++ src/main/kotlin/at/karriere/example/configshowcase/ConfigshowcaseApplication.kt
@@ -0,0 +1,11 @@
+package at.karriere.example.configshowcase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigshowcaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConfigshowcaseApplication>(*args)
+}
---
+++ src/test/kotlin/at/karriere/example/configshowcase/ConfigshowcaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.karriere.example.configshowcase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigshowcaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
